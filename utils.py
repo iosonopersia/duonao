ae2f94c922b6f0468faabee55e1d4da59ef8de47
iosonopersia <iosonopersia@gmail.com>
2020-12-04 13:35:34 +0100

    Fix do_moves

--- utils.py
+++ utils.py
@@ -15,13 +15,13 @@ def play_song(song_name):
 
 def do_moves(moves, robot_ip, robot_port):
     for move in moves:
-        print(f"Executing: {move}... ", end="")
+        print(f"Executing: {move}... ", end="", flush=True)
         python2_command = "python2 ./NaoMoves/"+move+".py " + str(robot_ip)+" " + str(robot_port)
         start_move = time.time()
         process = subprocess.run(python2_command.split(), stdout=subprocess.PIPE)
         move_length = time.time()-start_move
-        #print(process.stdout) # receive output from the python2 script
-        print(f"done in {move_length} seconds.")
+        # print(process.stdout) # receive output from the python2 script
+        print("done in %.2f seconds." % move_length, flush=True)
 
 
 def from_state_to_dict(state):
